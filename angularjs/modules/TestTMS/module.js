53d5ce8147e162d46f706db365e5200e7218c0ae
maoziPY <550632474@qq.com>
2016-06-15 00:33:21 +0800

    获取模板

--- angularjs/modules/TestTMS/module.js
+++ angularjs/modules/TestTMS/module.js
@@ -2,12 +2,18 @@
  * Created by PY on 2016/6/14 0014.
  */
 
+/**
+ * TestTMS 构造方法
+ * */
 function TestTMS() {
     var module = this;
-    module.container = $("." + arguments[0]);
-    module.name = /function\s+(\w+)/.exec(arguments.callee)[1]
+    module.container = $("." + arguments[0]); // 模块容器
+    module.name = /function\s+(\w+)/.exec(arguments.callee)[1]; // 模板名称
 }
 
+/**
+ * load 主方法
+ * */
 TestTMS.prototype.load = function () {
     var module = this;
 
@@ -15,6 +21,7 @@ TestTMS.prototype.load = function () {
     $.get("http://localhost:3000/getTemplate",
         {moduleName: module.name},
         function (data) {
+            // 模板渲染
             module.container.empty();
             module.container.append(data);
         }, "html");
